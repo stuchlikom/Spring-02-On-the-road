4902f7b501964f04fedb9e3db465d820891cc17f
Stuchlikom <stuchlikom@gmail.com>
2019-12-03 13:17:14 +0100

    ResponseEntity eingebaut

--- src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
+++ src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
 @Controller
 @SpringBootApplication
@@ -20,23 +21,23 @@ public class MyProjectApplication {
 	}
 
 	@RequestMapping("/doctor/{incarnation_number}")
-	@ResponseBody
-	public String incarnationnumber(@PathVariable String incarnation_number) {
+	public ResponseEntity<String> incarnation_number(@PathVariable String incarnation_number) {
 		switch (incarnation_number) {
 			case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8":
-				return "HTTP/1.1 303 See other.";
+				//return new ResponseEntity.status(303).body("See other.");
+				return ResponseEntity.status(303).body("HTTP/1.1 303 See other.");
 			case "9":
-				return "{\"number\": " + incarnation_number + ", \"name\": \"Christopher Eccleston\"}";
+				return ResponseEntity.status(200).body("{\"number\": " + incarnation_number + ", \"name\": \"Christopher Eccleston\"}");
 			case "10":
-				return "{\"number\": " + incarnation_number + ", \"name\": \"David Tennant\"}";
+				return ResponseEntity.status(200).body("{\"number\": " + incarnation_number + ", \"name\": \"David Tennant\"}");
 			case "11":
-				return "{\"number\": " + incarnation_number + ", \"name\": \"Matt Smith\"}";
+				return ResponseEntity.status(200).body("{\"number\": " + incarnation_number + ", \"name\": \"Matt Smith\"}");
 			case "12":
-				return "{\"number\": " + incarnation_number + ", \"name\": \"Peter Capaldi\"}";
+				return ResponseEntity.status(200).body("{\"number\": " + incarnation_number + ", \"name\": \"Peter Capaldi\"}");
 			case "13":
-				return "{\"number\": " + incarnation_number + ", \"name\": \"Jodie Whittaker\"}";
+				return ResponseEntity.status(200).body("{\"number\": " + incarnation_number + ", \"name\": \"Jodie Whittaker\"}");
 			default:
-				return "HTTP/1.1 404 Not found. Impossible to retrieve the incarnation " + incarnation_number + ".";
+				return ResponseEntity.status(404).body("HTTP/1.1 404 Not found. Impossible to retrieve the incarnation " + incarnation_number + ".");
 		}
 	}
 
